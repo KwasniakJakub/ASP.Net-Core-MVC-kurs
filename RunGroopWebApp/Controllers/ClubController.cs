6f68954f66626bf9b2ec54362ce40f3da217ea09
KwasniakJakub <jakub.kwasniak@wp.pl>
2023-05-30 19:14:47 +0200

    Depencency Injection + Repository Pattern

--- RunGroopWebApp/Controllers/ClubController.cs
+++ RunGroopWebApp/Controllers/ClubController.cs
@@ -1,26 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RunGroopWebApp.Data;
+using RunGroopWebApp.Interfaces;
 using RunGroopWebApp.Models;
 
 namespace RunGroopWebApp.Controllers
 {
     public class ClubController : Controller
     {
-        private readonly ApplicationDbContext _context;
+        private readonly IClubRepository _clubRepository;
 
-        public ClubController(ApplicationDbContext context)
+        public ClubController(IClubRepository clubRepository)
         {
-            _context = context;
+            _clubRepository = clubRepository;
         }
-        public IActionResult Index() //controller
+        public async Task<IActionResult> Index() //controller
         {
-            List<Club> clubs = _context.Clubs.ToList(); //model
+            IEnumerable<Club> clubs = await _clubRepository.GetAll(); //model
             return View(clubs); //view
         }
-        public IActionResult Detail(int id)
+        public async Task<IActionResult> Detail(int id)
         {
-            Club club = _context.Clubs.Include(a => a.Address).FirstOrDefault(c => c.Id == id);
+            Club club = await _clubRepository.GetByIdAsync(id) ;
             return View(club);
         }
     }
--- RunGroopWebApp/Controllers/RaceController.cs
+++ RunGroopWebApp/Controllers/RaceController.cs
@@ -1,27 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RunGroopWebApp.Data;
+using RunGroopWebApp.Interfaces;
 using RunGroopWebApp.Models;
 
 namespace RunGroopWebApp.Controllers;
 
 public class RaceController : Controller
 {
+    private readonly IRaceRepository _raceRepository;
     private readonly ApplicationDbContext _context;
 
-    public RaceController(ApplicationDbContext context)
+    public RaceController(IRaceRepository raceRepository)
     {
-        _context = context;
+        _raceRepository = raceRepository;
     }
     // GET
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        List<Race> races = _context.Races.ToList();
+        IEnumerable<Race> races = await _raceRepository.GetAll();
         return View(races);
     }
-    public IActionResult Detail(int id)
+    public async Task<IActionResult>  Detail(int id)
     {
-        Race race = _context.Races.Include(a => a.Address).FirstOrDefault(c => c.Id == id);
+        Race race = await _raceRepository.GetByIdAsync(id);
         return View(race);
     }
 }
